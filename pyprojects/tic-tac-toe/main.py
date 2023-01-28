d58d99952f9b5334badb00561ba8f4d12a385f85
trawwa <inekkit@gmail.com>
2023-01-28 22:07:32 +0700

    init board

---
+++ pyprojects/tic-tac-toe/main.py
@@ -0,0 +1,10 @@
+board = [' ' for x in range(10)]
+
+
+def printBoard(board):
+    print(" " + board[1] + "| " + board[2] + "| " + board[3] + "| ")
+    print("---------")
+    print(" " + board[4] + "| " + board[5] + "| " + board[6] + "| ")
+    print("---------")
+    print(" " + board[7] + "| " + board[8] + "| " + board[9] + "| ")
+    print("---------")
