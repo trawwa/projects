57174f55f6c15abe2d26c96d549cd15f1f7918f8
trawwa <inekkit@gmail.com>
2023-01-29 22:57:10 +0700

    def listUpdate and deleteTask

--- pyprojects/to-do-list/main.py
+++ pyprojects/to-do-list/main.py
@@ -14,3 +14,19 @@ def addTask():
         listUpdate()
         taskField.delete(0, 'end')
 
+
+def listUpdate():
+    clearList()
+    for task in tasks:
+        taskListbox.insert('end', task)
+
+
+def deleteTask():
+    try:
+        valueFromList = taskListbox.get(taskListbox.curselection())
+        if valueFromList in tasks:
+            tasks.remove(valueFromList)
+            listUpdate()
+            cursor.execute('delete from tasks where title = ?', (valueFromList,))
+    except:
+        messagebox.showinfo('Error!', 'No Task Selected. Cannot Delete.')
