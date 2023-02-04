4fc0d465bf19fcd972177005417fd2dc7209466a
trawwa <inekkit@gmail.com>
2023-02-04 11:52:22 +0700

    init ballon

---
+++ apk/balloon-game/main.lua
@@ -0,0 +1,11 @@
+background = display.newImage("background.png")
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+platform = display.newImage("platform.png", 0, 0)
+platform.x = display.contentCenterX
+platform.y = display.contentCenterY+250
+
+balloon = display.newImage("balloon.png", 0, 0)
+balloon.x = display.contentCenterX
+balloon.y = display.contentCenterY
