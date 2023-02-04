92668a1589a94f0702710372e8860713a4ce274d
trawwa <inekkit@gmail.com>
2023-02-04 12:51:38 +0700

    add physics

--- apk/balloon-game/main.lua
+++ apk/balloon-game/main.lua
@@ -9,3 +9,15 @@ platform.y = display.contentCenterY+250
 balloon = display.newImage("balloon.png", 0, 0)
 balloon.x = display.contentCenterX
 balloon.y = display.contentCenterY
+
+physics = require("physics")
+physics.start()
+
+physics.addBody(platform, "static")
+physics.addBody(balloon, "dynamic")
+
+function pushBalloon()
+    balloon:applyLinearImpulse(0, -0.75, balloon.x, balloon.y)
+end
+
+background:addEventListener("tap", pushBalloon)
