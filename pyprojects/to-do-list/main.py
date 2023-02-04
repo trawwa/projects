72adb0c410e16988e4dc255104642e62a18e2eb7
trawwa <inekkit@gmail.com>
2023-02-05 01:35:54 +0700

    upd main: corrects, init run

--- pyprojects/to-do-list/main.py
+++ pyprojects/to-do-list/main.py
@@ -10,7 +10,7 @@ def addTask():
         messagebox.showinfo('Error!', 'Field is empty.')
     else:
         tasks.append(taskString)
-        cursor.execute('Insert into task values (?)', (taskString ,))
+        cursor.execute('Insert into tasks values (?)', (taskString,))
         listUpdate()
         taskField.delete(0, 'end')
 
@@ -50,7 +50,7 @@ def close():
 
 
 def retrieveDatabase():
-    while(len(tasks) != 0):
+    while len(tasks) != 0:
         tasks.pop()
     for row in cursor.execute('select title from tasks'):
         tasks.append(row[0])
@@ -59,13 +59,13 @@ def retrieveDatabase():
 if __name__ == "__main__":
     guiWindow = Tk()
     guiWindow.title("To-Do List")
-    guiWindow.geometry("665x400+550+250")
+    guiWindow.geometry("550x400+550+250")
     guiWindow.resizable(0, 0)
     guiWindow.configure(bg="#B5E5CF")
 
     connection = sql.connect('listOfTasks.db')
     cursor = connection.cursor()
-    cursor.execute('create table if not exists tasks (little text)')
+    cursor.execute('create table if not exists tasks (title text)')
 
     tasks = []
 
@@ -102,7 +102,7 @@ if __name__ == "__main__":
                        command=deleteTask)
 
     delAllButton = Button(functionsFrame,
-                          text="Delete All Button",
+                          text="Delete All Tasks",
                           width=15,
                           bg='#D4AC0D',
                           font=('arial', '14', 'bold'),
@@ -110,14 +110,14 @@ if __name__ == "__main__":
 
     exitButton = Button(functionsFrame,
                         text='Exit',
-                        width=52,
+                        width=64,
                         bg='#D4AC0D',
                         font=('arial', '14', 'bold'),
                         command=close)
 
     addButton.place(x=18, y=80)
-    delButton.place(x=240, y=80)
-    delAllButton.place(x=460, y=80)
+    delButton.place(x=212, y=80)
+    delAllButton.place(x=403, y=80)
     exitButton.place(x=17, y=330)
 
     taskListbox = Listbox(functionsFrame,
@@ -128,4 +128,12 @@ if __name__ == "__main__":
                           background='WHITE',
                           foreground='BLACK',
                           selectbackground='#D4AC0D',
-                          selectgoreground='BLACK')
+                          selectforeground='BLACK')
+
+    taskListbox.place(x=17, y=140)
+
+    retrieveDatabase()
+    listUpdate()
+    guiWindow.mainloop()
+    connection.commit()
+    cursor.close()
