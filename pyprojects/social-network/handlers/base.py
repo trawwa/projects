da11441788b7f361d004af6559d710f23cac17af
trawwa <inekkit@gmail.com>
2023-05-15 13:42:22 +0700

    changes

--- pyprojects/social-network/handlers/base.py
+++ pyprojects/social-network/handlers/base.py
@@ -5,19 +5,21 @@ from aiohttp_session import get_session
 
 class Index(web.View):
 
+    @aiohttp_jinja2.template('index.html')
     async def get(self):
         conf = self.app['config']
-        return web.Response(text="Hello Aiohttp!")
+        return dict(conf=conf)
 
 
 class Login(web.View):
 
+    @aiohttp_jinja2.template('login.html')
     async def get(self):
         session = await get_session(self)
         session['last visit'] = str(datetime.utcnow())
         last_visit = session['last visit']
         text = 'Last visited: {}'.format(last_visit)
-        return web.Response(text="login Aiohttp!, {}".format(text))
+        return dict(text="Login Aiohttp!, {}".format(text))
 
     async def post(self):
         return web.Response(text="login aiohttp")
