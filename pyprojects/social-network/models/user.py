0b9e5480e5b7b07c608801f01506ae6e6002d0cf
trawwa <inekkit@gmail.com>
2023-05-18 21:48:41 +0700

    Enrichment

--- pyprojects/social-network/models/user.py
+++ pyprojects/social-network/models/user.py
@@ -1,10 +1,27 @@
-class User:
+import hashlib
+from motor.motor_asyncio import AsyncIOMotorDatabase
 
-    collection = None
+
+class User:
 
     def __init__(self):
         pass
 
+    @classmethod
+    async def get_user(cls, uid):
+        pass
+
     @staticmethod
-    async def get_user(db, uid):
-        return await db.users.find_one(uid)
+    async def create_new_user(db: AsyncIOMotorDatabase, data):
+        email = data['email']
+        user = await db.users.find_one({'email': email})
+        if user:
+            return dict(error='User with email {} is exist'.format(email))
+
+        if data['first_name'] and data['last_name'] and data['password']:
+            data = dict(data)
+            data['password'] = hashlib.sha256(data['password'].encode(utf-8)).hexdigest()
+            result = await db.users.insert_one(data)
+            return result
+        else:
+            return dict(error='Missing user data parameters')
