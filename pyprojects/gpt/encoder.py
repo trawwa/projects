1689d30682dc3ef7738960f47aa05ac9a887136a
trawwa <inekkit@gmail.com>
2023-07-23 22:06:54 +0700

    delete bullshit copy and just cloned

--- pyprojects/gpt/encoder.py
+++ pyprojects/gpt/encoder.py
@@ -1,23 +1,41 @@
+"""Byte pair encoding utilities.
+
+Copied from: https://github.com/openai/gpt-2/blob/master/src/encoder.py.
+"""
 import json
 import os
 from functools import lru_cache
 
 import regex as re
 
+
 @lru_cache()
 def bytes_to_unicode():
-    bs = list(range(ord("!"), ord("~") + 1)) +list(range(ord("i"), ord("¬") + 1)) + list(range(ord("®"), ord("ÿ") + 1))
+    """
+    Returns list of utf-8 byte and a corresponding list of unicode strings.
+    The reversible bpe codes work on unicode strings.
+    This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
+    When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
+    This is a significant percentage of your normal, say, 32K bpe vocab.
+    To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
+    And avoids mapping to whitespace/control characters the bpe code barfs on.
+    """
+    bs = list(range(ord("!"), ord("~") + 1)) + list(range(ord("¡"), ord("¬") + 1)) + list(range(ord("®"), ord("ÿ") + 1))
     cs = bs[:]
     n = 0
     for b in range(2**8):
         if b not in bs:
             bs.append(b)
             cs.append(2**8 + n)
-            n += 1 
+            n += 1
     cs = [chr(n) for n in cs]
     return dict(zip(bs, cs))
 
+
 def get_pairs(word):
+    """Return set of symbol pairs in a word.
+    Word is represented as tuple of symbols (symbols being variable-length strings).
+    """
     pairs = set()
     prev_char = word[0]
     for char in word[1:]:
@@ -25,16 +43,19 @@ def get_pairs(word):
         prev_char = char
     return pairs
 
+
 class Encoder:
     def __init__(self, encoder, bpe_merges, errors="replace"):
         self.encoder = encoder
         self.decoder = {v: k for k, v in self.encoder.items()}
-        self.errors = errors 
+        self.errors = errors  # how to handle errors in decoding
         self.byte_encoder = bytes_to_unicode()
         self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
         self.bpe_ranks = dict(zip(bpe_merges, range(len(bpe_merges))))
         self.cache = {}
-        self.pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?[^\s\p{L}\p{N}] +|\s+(?!\S) |\s+""")
+
+        # Should have added re.IGNORECASE so BPE merges can happen for capitalized versions of contractions
+        self.pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
 
     def bpe(self, token):
         if token in self.cache:
@@ -44,7 +65,7 @@ class Encoder:
 
         if not pairs:
             return token
-        
+
         while True:
             bigram = min(pairs, key=lambda pair: self.bpe_ranks.get(pair, float("inf")))
             if bigram not in self.bpe_ranks:
@@ -63,37 +84,37 @@ class Encoder:
 
                 if word[i] == first and i < len(word) - 1 and word[i + 1] == second:
                     new_word.append(first + second)
-                    i +=2
+                    i += 2
                 else:
                     new_word.append(word[i])
-                    i +=1
+                    i += 1
             new_word = tuple(new_word)
             word = new_word
-            if len(word) ==1:
+            if len(word) == 1:
                 break
             else:
                 pairs = get_pairs(word)
         word = " ".join(word)
         self.cache[token] = word
         return word
-    
+
     def encode(self, text):
         bpe_tokens = []
         for token in re.findall(self.pat, text):
             token = "".join(self.byte_encoder[b] for b in token.encode("utf-8"))
             bpe_tokens.extend(self.encoder[bpe_token] for bpe_token in self.bpe(token).split(" "))
         return bpe_tokens
-    
+
     def decode(self, tokens):
         text = "".join([self.decoder[token] for token in tokens])
         text = bytearray([self.byte_decoder[c] for c in text]).decode("utf-8", errors=self.errors)
         return text
-    
+
 
 def get_encoder(model_name, models_dir):
     with open(os.path.join(models_dir, model_name, "encoder.json"), "r") as f:
         encoder = json.load(f)
     with open(os.path.join(models_dir, model_name, "vocab.bpe"), "r", encoding="utf-8") as f:
         bpe_data = f.read()
-    bpe_merges = [tuple(merge_str.split()) for merge_str in bpe_data.split("\n") [1:-1]]
+    bpe_merges = [tuple(merge_str.split()) for merge_str in bpe_data.split("\n")[1:-1]]
     return Encoder(encoder=encoder, bpe_merges=bpe_merges)
--- pyprojects/gpt/utils.py
+++ pyprojects/gpt/utils.py
@@ -5,7 +5,7 @@ import re
 import numpy as np
 import requests
 import tensorflow as tf
-import tqdm
+from tqdm import tqdm
 
 from encoder import get_encoder
 
@@ -17,8 +17,8 @@ def download_gpt2_files(model_size, model_dir):
         "encoder.json",
         "hparams.json",
         "model.ckpt.data-00000-of-00001",
-        "model.ckpt.meta",
         "model.ckpt.index",
+        "model.ckpt.meta",
         "vocab.bpe",
     ]:
         url = "https://openaipublic.blob.core.windows.net/gpt-2/models"
@@ -29,10 +29,11 @@ def download_gpt2_files(model_size, model_dir):
             file_size = int(r.headers["content-length"])
             chunk_size = 1000
             with tqdm(
-                ncols = 100,
+                ncols=100,
                 desc="Fetching " + filename,
                 total=file_size,
                 unit_scale=True,
+                unit="b",
             ) as pbar:
                 # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
                 for chunk in r.iter_content(chunk_size=chunk_size):
@@ -40,7 +41,7 @@ def download_gpt2_files(model_size, model_dir):
                     pbar.update(chunk_size)
 
 
-def load_gpt2_params_from_tf_checkpoint(tf_checkpoint_path, hparams):
+def load_gpt2_params_from_tf_ckpt(tf_ckpt_path, hparams):
     def set_in_nested_dict(d, keys, val):
         if not keys:
             return val
@@ -49,10 +50,9 @@ def load_gpt2_params_from_tf_checkpoint(tf_checkpoint_path, hparams):
         d[keys[0]] = set_in_nested_dict(d[keys[0]], keys[1:], val)
         return d
 
-    #init_vars = tf.train.list_variables(tf_checkpoint_path)
     params = {"blocks": [{} for _ in range(hparams["n_layer"])]}
-    for name, _ in tf.train.list_variables(tf_checkpoint_path):
-        array = np.squeeze(tf.train.load_variable(tf_checkpoint_path, name))
+    for name, _ in tf.train.list_variables(tf_ckpt_path):
+        array = np.squeeze(tf.train.load_variable(tf_ckpt_path, name))
         name = name[len("model/") :]
         if name.startswith("h"):
             m = re.match(r"h([0-9]+)/(.*)", name)
@@ -69,14 +69,14 @@ def load_encoder_hparams_and_params(model_size, models_dir):
     assert model_size in ["124M", "355M", "774M", "1558M"]
 
     model_dir = os.path.join(models_dir, model_size)
-    tf_checkpoint_path = tf.train.latest_checkpoint(model_dir)
-    if not tf_checkpoint_path: # download files if necessary
+    tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
+    if not tf_ckpt_path:  # download files if necessary
         os.makedirs(model_dir, exist_ok=True)
         download_gpt2_files(model_size, model_dir)
-        tf_checkpoint_path = tf.train.latest_checkpoint(model_dir)
+        tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
 
-    encoder = get_encoder(model_size, model_dir)
-    hparams = json.load(open(os.path.join(models_dir, "hparams.json")))
-    params = load_gpt2_params_from_tf_checkpoint(tf_checkpoint_path, hparams)
+    encoder = get_encoder(model_size, models_dir)
+    hparams = json.load(open(os.path.join(model_dir, "hparams.json")))
+    params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, hparams)
 
     return encoder, hparams, params
