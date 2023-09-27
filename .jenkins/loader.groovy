0dccf57b2eb47061c47cda991359e63b65d22814
trawwa <43407282+trawwa@users.noreply.github.com>
2023-09-27 21:17:20 +0700

    init loader

---
+++ .jenkins/loader.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+    parametres {
+        string(name: "promt", defaultValue: "", description: "Здесь нужно ввести запрос к GPT.")
+    }
+    stages {
+        stage ("Загрузка скрипта") {
+            step {
+                def gpt2 
+                fileLoader.withGit('https://github.com/trawwa/projects.git', 'main', null, '') {
+                    gpt2 = fileLoader.load('pyprojects/gpt/gpt2.py');
+                }
+            }
+        }
+        stage ("Установка зависимостей") {
+            sh "pip install -r requirements.txt"
+        }
+        stage ("Выполнение скрипта") {
+            step {
+                gpt2.main()
+            }
+        }
+    }
+}
