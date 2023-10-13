31db845f5198d99ae4dd4e4b8786ae6df1e9b0c9
trawwa <43407282+trawwa@users.noreply.github.com>
2023-10-13 07:55:45 +0700

    }

--- .jenkins/loader.groovy
+++ .jenkins/loader.groovy
@@ -4,16 +4,16 @@ pipeline {
         string(name: 'promt', defaultValue: '', description: 'Здесь нужно ввести запрос к GPT.')
     }
     stages {
-        stage("Загрузка скрипта") {
-            steps {
-                script {
-                    // def gpt2 
-                    // fileLoader.withGit('https://github.com/trawwa/projects.git', 'main', null, '') {
-                    //     gpt2 = load('pyprojects/gpt/gpt2.py');
-                    }  
-                }
-            }
-        }
+        // stage("Загрузка скрипта") {
+        //     steps {
+        //         script {
+        //             // def gpt2 
+        //             // fileLoader.withGit('https://github.com/trawwa/projects.git', 'main', null, '') {
+        //             //     gpt2 = load('pyprojects/gpt/gpt2.py');
+        //             }  
+        //         }
+        //     }
+        
         stage("Установка зависимостей") {
             steps {
                 script{
@@ -30,6 +30,7 @@ pipeline {
             }
         }
     }
+}
     post {
         always {
             cleanWs()
