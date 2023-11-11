fe7369871072ec6e971f9e43b0145acf6f4f20b5
trawwa <inekkit@gmail.com>
2023-11-11 19:28:10 +0700

    some changes

--- pyprojects/social-network/handlers/base.py
+++ pyprojects/social-network/handlers/base.py
@@ -42,5 +42,24 @@ class Login(web.View):
 
 class Signup(web.View):
 
+    @aiohttp_jinja2.template('signup.html')
     async def get(self):
-        return web.Response(text="signup Aiohttp!")
+        session = await get_session(self)
+        session['last visit'] = str(datetime.utcnow())
+        last_visit = session['last visit']
+
+        db = self.app['db']
+        user = await User.get_user(uid=1)
+        document = await db.test.find_one()
+        return dict(text="Login Aiohttp!, Last Visited: {}".format(last_visit))
+
+    async def post(self):
+        data = await self.post()
+        login = data['login']
+        password = data['password']
+
+        session = await get_session(self)
+        session['user'] = {"login": login}
+
+        location = self.app.router['index'].url_for()
+        return web.HTTPFound(location=location)
