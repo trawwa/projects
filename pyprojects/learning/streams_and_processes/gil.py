79227f008e6221288b754adfc8602b32cad183ef
trawwa <43407282+trawwa@users.noreply.github.com>
2024-03-02 22:13:49 +0700

    Threading

--- pyprojects/learning/streams_and_processes/gil.py
+++ pyprojects/learning/streams_and_processes/gil.py
@@ -1,1 +1,16 @@
+import threading
+import time 
 
+def thread_function(name):
+    print(name, " - thread starting")
+    time.sleep(2)
+    print(name, " - after sleep")
+
+if __name__ == "__main__":
+    print("before create Thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    print("before running Thread")
+    x.start()
+    print("Wait thread finish")
+    ##x.join()
+    print("all done")
