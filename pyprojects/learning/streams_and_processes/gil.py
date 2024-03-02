a2ab7fa254b3579addb7b580751f944051581b0d
trawwa <43407282+trawwa@users.noreply.github.com>
2024-03-02 22:29:47 +0700

    Go

--- pyprojects/learning/streams_and_processes/gil.py
+++ pyprojects/learning/streams_and_processes/gil.py
@@ -7,8 +7,10 @@ def thread_function(name):
     print(name, " - after sleep")
 
 if __name__ == "__main__":
-    print("before create Thread")
-    x = threading.Thread(target=thread_function, args=(1,))
+    threads = []
+    for index in range(3):
+        print("create Thread - ", index)
+        x = threading.Thread(target=thread_function, args=(1,))
     print("before running Thread")
     x.start()
     print("Wait thread finish")
