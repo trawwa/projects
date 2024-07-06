7cfb07bb67530808e50d759c9849e88e8948b82f
trawwa <inekkit@gmail.com>
2024-07-06 18:44:31 +0700

    upd but have 500 with age

--- js/client-server-qraphql-example/client/src/App.js
+++ js/client-server-qraphql-example/client/src/App.js
@@ -1,7 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ALL_USERS } from './query/user';
+import { GET_ONE_USER } from './query/user';
+import { CREATE_USER } from './mutations/user';
 
 function App() {
+  const {data, loading, error, refetch} = useQuery(GET_ALL_USERS)
+  const {data:oneUser, loading: loadingOneUser} = useQuery(GET_ONE_USER, {
+    variables: {
+      id: 1
+    }
+  })
+  const [newUser] = useMutation(CREATE_USER)
+  const [users, setUsers] = useState([])
+  const [username, setUsername] = useState('')
+  const [age, setAge] = useState(0)
+  
+  console.log(oneUser)
+  
+  useEffect(() => {
+    if (!loading) {
+      setUsers(data.getAllUsers)
+    }
+  }, [data])
+
+  const addUser = e => {
+    e.preventDefault()
+    newUser({
+      variables: {
+        input: {
+          username, age
+        }
+      }
+    }).then(({data}) => {
+      console.log(data)
+      setUsername('')
+      setAge()
+      
+    })
+  }
+  const getAll = e => {
+    e.preventDefault()
+    refetch()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +61,22 @@ function App() {
         >
           Learn React
         </a>
+        <form>
+          <input value={username} onChange={e => setUsername(e.target.value)} type="text"/>
+          <input value={age} onChange={e => setAge(e.target.value)} type="number"/>
+          <div className='btns'>
+            <button onClick={e => addUser(e)}>Создать</button>
+            <button onClick={e => getAll(e)}>Получить</button>
+          </div>
+      </form>
+      <div>
+        {users.map(user =>
+          <div className='user'>{user.id}. {user.username}. {user.age}</div>
+        )}
+      </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- js/client-server-qraphql-example/client/src/index.js
+++ js/client-server-qraphql-example/client/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient( {
+  uri: 'http://localhost:5050/graphql',
+  cache: new InMemoryCache(),
+  introspection: true
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
