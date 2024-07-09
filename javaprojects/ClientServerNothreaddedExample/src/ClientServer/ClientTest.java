aafa8c55e5d879b8acb666cead6a34d9a35698a7
trawwa <inekkit@gmail.com>
2024-07-09 23:55:48 +0700

    init client

---
+++ javaprojects/ClientServerNothreaddedExample/src/ClientServer/ClientTest.java
@@ -0,0 +1,80 @@
+package javaprojects.ClientServerNothreaddedExample.src.ClientServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+import javaprojects.ClientServerNothreaddedExample.src.ClientServer.MessageUtils;
+
+public class ClientTest implements Runnable {
+    ServerSocket serverSocket;
+    volatile boolean keepProcessing = true;
+
+    public ClientTest(int port, int millisecondsTiomeout) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(millisecondsTiomeout);
+    }
+
+    public void run() {
+        System.out.println("Server Starting");
+
+        while (keepProcessing) {
+            try {
+                System.out.println("accepting client");
+                Socket socket = serverSocket.accept();
+                System.out.println("got client");
+                proccess(socket);
+            } catch (Exception e) {
+                handle(e);
+            }
+        }
+    }
+
+    private void handle(Exception e) {
+        if (!(e instanceof SocketException)) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopProcessing() {
+        keepProcessing = false;
+        closeIgnoringException(serverSocket);
+    }
+
+    void proccess(Socket socket) {
+        if (socket == null) {
+            return;
+        }
+
+        try { 
+            System.out.println("Server: getting message");
+            String message = MessageUtils.getMessage(socket);
+            System.out.printf("Server: got message:", message);
+            Thread.sleep(1000);
+            System.out.printf("Server: sending reply:", message);
+            MessageUtils.sendMessage(socket, "Processed: " + message);
+            System.out.println("Server: sent");
+            closeIgnoringException(socket);
+        } catch (Exception e) { 
+            e.printStackTrace();
+        }
+
+    }
+
+    private void closeIgnoringException(Socket socket) {
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException ignore) {}
+        }
+    }
+
+    private void closeIgnoringException(ServerSocket serverSocket) {
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException ignore) {}
+        }
+    }
+}
