2523c06e761ae3e52c8870cb98cb618960c29596
trawwa <inekkit@gmail.com>
2024-12-08 02:19:05 +0700

    RESEARCH-3: model_validator

--- pyprojects/learning/pydantic/test_pydantic.py
+++ pyprojects/learning/pydantic/test_pydantic.py
@@ -1,56 +1,66 @@
-from pydantic import BaseModel, field_validator, model_validator
+from pydantic import BaseModel, field_validator, model_validator, computed_field, Field
 from datetime import date
+from dateutil.relativedelta import relativedelta
+from uuid import uuid4
+
 
 class User(BaseModel):
-    id: int
-    name: str
-    birthday_date: date
+    id: int = Field(
+        default=1,
+        description="Уникальный идентификатор пользователя"
+        )
+    name: str = Field(
+        min_length=3,
+        max_length=20,
+        default="John Doe",
+        title="Имя пользователя",
+        description="Полное имя"
+        )
+    role: str = Field(
+        default="user",
+        alias="user_role",
+        description="Роль пользователя в системе"
+        )
+    email: str = Field(
+        pattern=r"[^@]+@[^@]+\.[^@]+",
+        description="Электронная почта должна быть в корректном формате"
+        )
+    phone_number: str = Field(
+        pattern=r"^\+\d{1,3}\s?\d{4,14}$",
+        description="Номер телефона должен быть в формате +123456789"
+        )
+    username: str = Field(
+        alias="user_name"
+        )
+    password: str = Field(
+        exclude=True
+        )
+    
 
-    @field_validator('name', mode='before')
-    def validate_name(cls, v):
-        if isinstance(v, int):
-            return str(v)
-        elif isinstance(v, str):
-            return v
-        else:
-            raise ValueError("Имя должно быть строкой или числом")
+class Product(BaseModel):
+    price: float = Field(
+        gt=0,
+        description="Цена должна быть больше нуля"
+        )
+    name: str = Field(
+        min_length=2,
+        max_length=50,
+        description="Название продукта должно быть от 2 до 50 символов"
+        )
+    rating: int = Field(
+        ge=1,
+        le=5,
+        description="Рейтинг должен быть от 1 до 5")
+    
 
-    @model_validator(mode='after')
-    def check_age(self):
-        today = date.today()
-        age = today.year - self.birthday_date.year - (
-            (today.month, today.day) < (self.birthday_date.month, self.birthday_date.day))
+class Item(BaseModel):
+    id: str = Field(
+        default_factory=lambda: uuid4().hex
+        )
+    
 
-        if age < 18:
-            raise ValueError("Пользователь должен быть старше 18 лет")
-        if age > 120:
-            raise ValueError("Возраст не может превышать 120 лет")
-        return self
-
-    @model_validator(mode='after')
-    def set_default_name(self):
-        if self.name.strip() == '':
-            self.name = f"User_{self.id}"
-        return self  
-
-
-try:
-    user = User(id=1, name="John", birthday_date=date(2000, 1, 1))
-    print(user)
-except ValueError as e:
-    print(f"Ошибка: {e}")
-
-try:
-    user = User(id=2, name="", birthday_date=date(2020, 1, 1))
-    print(user)
-except ValueError as e:
-    print(f"Ошибка: {e}")
-
-try:
-    user = User(id=3, name="Alice", birthday_date=date(1900, 1, 1))
-    print(user)
-except ValueError as e:
-    print(f"Ошибка: {e}")
+class Config(BaseModel):
+    debug_mode: bool = Field(repr=False)
 
 
 
@@ -87,3 +97,69 @@ except ValueError as e:
 
 # print(to_dict, type(to_dict))
 # print(to_json, type(to_json)) 
+
+#     @field_validator('name', mode='before')
+#     def validate_name(cls, v):
+#         if isinstance(v, int):
+#             return str(v)
+#         elif isinstance(v, str):
+#             return v
+#         else:
+#             raise ValueError("Имя должно быть строкой или числом")
+
+#     @model_validator(mode='after')
+#     def check_age(self):
+#         today = date.today()
+#         age = today.year - self.birthday_date.year - (
+#             (today.month, today.day) < (self.birthday_date.month, self.birthday_date.day))
+
+#         if age < 18:
+#             raise ValueError("Пользователь должен быть старше 18 лет")
+#         if age > 120:
+#             raise ValueError("Возраст не может превышать 120 лет")
+#         return self
+
+#     @model_validator(mode='after')
+#     def set_default_name(self):
+#         if self.name.strip() == '':
+#             self.name = f"User_{self.id}"
+#         return self  
+
+
+# try:
+#     user = User(id=1, name="John", birthday_date=date(2000, 1, 1))
+#     print(user)
+# except ValueError as e:
+#     print(f"Ошибка: {e}")
+
+# try:
+#     user = User(id=2, name="", birthday_date=date(2020, 1, 1))
+#     print(user)
+# except ValueError as e:
+#     print(f"Ошибка: {e}")
+
+# try:
+#     user = User(id=3, name="Alice", birthday_date=date(1900, 1, 1))
+#     print(user)
+# except ValueError as e:
+#     print(f"Ошибка: {e}")
+
+#     @computed_field
+#     def full_name(self) -> str:
+#         return f"{self.name} {self.surname}"
+    
+#     @computed_field
+#     def age(self) -> str:
+#         today = date.today()
+#         delta = relativedelta(today, self.birthday_date)
+#         return f"{delta.years} лет, {delta.months} месяцев и {delta.days} дней."
+
+
+# alex = User(
+#     id=1,
+#     name="Name",
+#     surname="Surname",
+#     birthday_date="1998-03-26"
+# )
+
+# print(alex.dict())
